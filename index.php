d3484691eac3acacbb0088aa947b0f354954cfc0
Ankit Singh <ankit20893@gmail.com>
2016-05-02 20:02:21 +0530

    social site

---
+++ index.php
@@ -0,0 +1,44 @@
+<!<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Welcome to the new Social world!</h1>
+        <p>Share your experiences!</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <h3><a href="index.php">Register yourself </a></h3>
+
+        </div>
+        <div class="col-sm-6">
+            <h3><a href="index.php">Login</a></h3>
+            </div>
+
+    </div>
+</div>
+
+
+</body>
+</html>>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 02-05-2016
+ * Time: 18:34
+ */
---
+++ registration.php
@@ -0,0 +1,17 @@
+<!<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>registration</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 02-05-2016
+ * Time: 19:22
+ */
